234ba7916360086fed90f99b99537ea3c55bee89
segward <gustavhaverstad@gmail.com>
2026-02-21 02:58:46 +0100

    testing drawing several sprites on top of each other

--- src/engine/window.c
+++ src/engine/window.c
@@ -27,8 +27,8 @@ bool window_init(int width, int height, const char *title)
     return false;
   }
 
-  glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glEnable(GL_BLEND);
 
   global.window.handle = handle;
   global.window.width = width;
--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ int main(void) {
 
   while (!glfwWindowShouldClose(global.window.handle)) {
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     double current_time = glfwGetTime();
     if (current_time - last_time >= frame_delay) {
@@ -30,6 +30,10 @@ int main(void) {
 
     quad_draw((vec2){ 400, 400 }, (vec2){ 400, 400 }, 
               (vec4){ 1.0f, 1.0f, 1.0f, 1.0f}, 
+              animation.texture.id, animation.uv);
+
+    quad_draw((vec2){ 300, 200 }, (vec2){ 300, 300 }, 
+              (vec4){ 0.5f, 0.5f, 1.0f, 1.0f}, 
               animation.texture.id, animation.uv);
 
     glfwSwapBuffers(global.window.handle);
